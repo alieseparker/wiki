60c9675787d3ed8b8693551af641f063fde2802f
Forrest Parker <forresta.parker@gmail.com>
2014-11-10 12:52:47 -0800

    all tests pass

---
+++ app/views/recipes/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @recipe, :id, :title, :steps, :created_at, :updated_at
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,9 +3,17 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'minitest/rails'
 require 'minitest/rails/capybara'
-class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+class ActionController::TestCase
+  include Devise::TestHelpers
+end
+
+class ActionDispatch::IntegratoinTest
+  include Rails.application.routes.url_helpers
+  include Capybara::DSL
+end
+
+class ActiveSupport::TestCase
+  ActiveRecord::Migration.check_pending!
+  fixtures :all
 end
